3083cb7311b09d21f2b6a6010ca3d2da3c582939
igor317 <44530414+igor317@users.noreply.github.com>
2019-11-21 23:40:55 +0300

    Добавьте файлы проекта.

---
+++ CardReader/Form1.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CardReader
+{
+    public partial class Form1 : Form
+    {
+        private IpPorts Port;
+
+        private void UpdatePortList()
+        {
+            listBox1.Items.Clear();
+
+            String[] v = Port.GetPortList();
+
+            for (int i = 0; i < v.Length; ++i)
+                listBox1.Items.Add(v[i]);
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+            Port = new IpPorts(9600);
+            Port.IpPortDataRecieved += Port_IpPortDataRecieved;
+
+        }
+
+        private void Port_IpPortDataRecieved(object sender, IpPortsEventArgs e)
+        {
+            richTextBox1.Invoke(new Action(() => richTextBox1.Text = e.Data));
+           // richTextBox1.Text = e.Data;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            UpdatePortList();
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string PortName = listBox1.Items[listBox1.SelectedIndex].ToString();
+            if (Port.OpenPort(PortName))
+                listBox2.Items.Add(PortName + " successfully opened");
+            else
+                listBox2.Items.Add("Cannot open port");
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (Port.ClosePort())
+                listBox2.Items.Add(Port.GetPortName() + " successfully closed");
+            else
+                listBox2.Items.Add("Cannot close port");
+        }
+    }
+}
---
+++ CardReader/IpPorts.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO.Ports;
+
+namespace CardReader
+{
+    class IpPortsEventArgs
+    {
+        public string Data { get; }
+        public IpPortsEventArgs(string Data)
+        {
+            this.Data = Data;
+        }
+    }
+
+
+    class IpPorts
+    {
+        #region PRIVATE METHODS
+        private SerialPort m_Port;
+        private string m_Buffer;
+        private bool m_ReadData;
+
+        public delegate void IpDataRecieved(object sender, IpPortsEventArgs e);
+        public event IpDataRecieved IpPortDataRecieved;
+
+       // private string 
+
+        private void M_Port_DataReceived(object sender, SerialDataReceivedEventArgs e)
+        {
+            if (!m_ReadData)
+                return;
+
+            SerialPort port = (SerialPort)sender;
+
+            m_Buffer += port.ReadExisting();
+
+            if (IpPortDataRecieved != null)
+                IpPortDataRecieved(this, new IpPortsEventArgs(m_Buffer));
+        }
+
+        #endregion
+
+        public IpPorts(int BaudRate)
+        {
+            m_ReadData = true;
+            m_Port = new SerialPort();
+            m_Port.BaudRate = BaudRate;
+            m_Port.DataReceived += M_Port_DataReceived;
+        }
+
+        public void SetBaudRate(int BaudRate)
+        {
+            m_Port.BaudRate = BaudRate;
+        }
+
+        public String[] GetPortList()
+        {
+            return SerialPort.GetPortNames();
+        }
+
+        public bool OpenPort(string portName)
+        {
+            try
+            {
+                m_Port.PortName = portName;
+                m_Port.Open();
+                return true;
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+        }
+
+        public bool ClosePort()
+        {
+            if (!m_Port.IsOpen)
+                return false;
+            m_Port.Close();
+            return true;
+        }
+
+        public void SetReadState(bool state)
+        {
+            m_ReadData = state;
+        }
+
+        public String GetBuffer()
+        {
+            return m_Buffer;
+        }
+
+        public string GetPortName()
+        {
+            return m_Port.PortName;
+        }
+       // public void ClearBuffer()
+    }
+}
